724f8b5e99b6b0a33ad0f8c7b07e38f889cd5785
a5hil <ashil555anto@gmail.com>
2026-07-14 20:59:56 +0530

    add-team API done

--- app.js
+++ app.js
@@ -33,3 +33,12 @@ const Team = mongoose.model('Team', new mongoose.Schema(
         stationNumber: Number
     }
 ));
+
+app.post('/add-team', async (req, res) => {
+    await Team.create(req.body);
+    res.json({'status': 'success'});
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
