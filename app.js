beb47875bfdaa2949c7040fb6f1f582606ae7fea
a5hil <ashil555anto@gmail.com>
2026-07-14 20:52:24 +0530

    db connection done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://ashil:Superman1275@ac-gawvbv0-shard-00-00.pzyiuyr.mongodb.net:27017,ac-gawvbv0-shard-00-01.pzyiuyr.mongodb.net:27017,ac-gawvbv0-shard-00-02.pzyiuyr.mongodb.net:27017/hackathondb?ssl=true&replicaSet=atlas-mzsy2b-shard-0&authSource=admin&appName=Cluster0').then(
+    () => {
+        console.log('Connected to MongoDB');
+    }
+).catch(
+    (error) => {
+        console.error('Error connecting to MongoDB:', error);
+    }
+)
+
+const Team = mongoose.model('Team', new mongoose.Schema(
+    {
+        teamId: String,
+        teamName: String,
+        teamLeaderName: String,
+        leaderEmail: String,
+        leaderPhone: Number,
+        collegeName: String,
+        numberOfMembers: Number,
+        projectTitle: String,
+        problemStatement: String,
+        technologyStack: String,
+        mentorName: String,
+        registrationDate: String,
+        stationNumber: Number
+    }
+));
